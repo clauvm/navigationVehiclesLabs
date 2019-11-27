52a83f5d5d1aacf44c69f9e09ad2c46ff642840b
nik1168 <ngeisser32@gmail.com>
2019-11-27 17:32:15 +0100

    Implementing Measurement update phase and corrector phase with respective equations and gaussian probability distribution function

--- kalman_filter/main.py
+++ kalman_filter/main.py
@@ -1,13 +1,39 @@
-from kalman_filter.statsUtils import update, predict, gaussian_function
+"""
+Kalman filter implementation was based on paper "Implementation of Kalman Filter with Python Language"
+Mohamed LAARAIEDH
+IETR Labs, University of Rennes 1
+Mohamed.laaraiedh@univ-rennes1.fr
+
+Code was adapted to follow the notation of the course assignment
+"""
+from numpy.linalg import inv
+
+from kalman_filter.statsUtils import gauss_pdf
 import matplotlib.pyplot as plt
 import numpy as np
 
 
 def process_equation(previous_state, A, B, G, control_input, process_noise):
+    """
+    Desbribe the process equation
+    :param previous_state:
+    :param A:
+    :param B:
+    :param G:
+    :param control_input:
+    :param process_noise:
+    :return:
+    """
     return A * previous_state + B * control_input + G * process_noise
 
 
 def generate_data():
+    """
+    Generates data for the measurements, 50 records subject to a noise that follows a normal
+    probability distribution mu = 0 and standard deviation of 0.1, this function also plots
+    the data
+    :return: measurements
+    """
     # We will choose a scalar constant
     x = 0.26578
     mu = 0
@@ -20,13 +46,53 @@ def generate_data():
     print(measurements)
     print("np mean")
     print(mean)
-    plt.plot(measurements,'+',color='r',label='measurements')
-    plt.axhline(mean, color='black', linestyle='--',label='mean value of measurements')
+    plt.plot(measurements, '+', color='r', label='measurements')
+    plt.axhline(mean, color='black', linestyle='--', label='mean value of measurements')
     plt.ylabel('voltage [V]')
     plt.xlabel('Measurement')
     plt.legend()
     plt.show()
+    return measurements
 
+
+def prediction_step(A, x_hat_previous, B, control_input, Q, P_previous):
+    """
+    Computes prediction of mean (X) and covariance(P) of the system at a specific timestep
+    These are the time update equations on the predictor phase
+    :param A: The transition n n × matrix (In this case is 1)
+    :param x_hat_previous: The mean state estimate of the previous step (k - 1)
+    :param B: The input effect matrix. Since input control is 0 this will be 0
+    :param control_input: In this case is 0
+    :param Q: The process noise covariance matrix
+    :param P_previous: The state covariance of previous step
+    :return: predicted mean(x_hat) and predicted covariance (P_hat)
+    """
+    x_hat = np.dot(A, x_hat_previous) + np.dot(B, control_input)
+    P_hat = np.dot(A, np.dot(P_previous, A.T)) + Q
+    return (x_hat, P_hat)
+
+
+def update_step(x_hat, P_hat, Y, C, R):
+    """
+    Computes the posterior mean X and covariance P of the system state given a new measurement at time step k
+    This is the measurement update phase or the corrector
+    :param x_hat: predicted mean(x_hat)
+    :param P_hat: predicted covariance (P_hat)
+    :param Y: measurement vector
+    :param C: measurement matrix
+    :param R: covariance matrix
+    :return:
+    """
+    IM = np.dot(C, x_hat)  # the Mean of predictive distribution of Y
+    IS = R + np.dot(C, np.dot(P_hat, C.T))  # the Covariance or predictive mean of Y
+    K = np.dot(P_hat, np.dot(C.T, inv(IS)))  # Kalman Gain matrix
+    X = x_hat + np.dot(K, (Y - IM))
+    P = P_hat - np.dot(K, np.dot(IS, K.T))
+    LH = gauss_pdf(Y, IM, IS)  # the Predictive probability (likelihood)
+    return (X, P, K, IM, IS, LH)
+
+
+def kalman_filter():
     pass
 
 
@@ -78,5 +144,4 @@ if __name__ == "__main__":
     """
     In this step, we will generate the data
     """
-    generate_data()
-
+    measurements = generate_data()
--- kalman_filter/statsUtils.py
+++ kalman_filter/statsUtils.py
@@ -1,11 +1,13 @@
 import math
+import numpy as np
+from numpy.linalg import inv
 
 
 def gaussian_function(mu, sigma_squared, x):
     ''' f takes in a mean and squared variance, and an input x
        and returns the gaussian value.'''
-    coefficient = 1.0 / math.sqrt(2.0 * math.pi *sigma_squared)
-    exponential = math.exp(-0.5 * (x-mu) ** 2 / sigma_squared)
+    coefficient = 1.0 / math.sqrt(2.0 * math.pi * sigma_squared)
+    exponential = math.exp(-0.5 * (x - mu) ** 2 / sigma_squared)
     return coefficient * exponential
 
 
@@ -29,3 +31,29 @@ def predict(mean1, var1, mean2, var2):
     new_var = var1 + var2
 
     return [new_mean, new_var]
+
+
+def gauss_pdf(X, M, S):
+    """
+    Computes the Predictive probability (likelihood) of measurement
+    :param X: measurement vector
+    :param M: Mean of predictive distribution of measurement vector
+    :param S: the Covariance or predictive mean of measurement vector
+    :return: Gaussian probability distribution
+    """
+    if M.shape()[1] == 1:
+        DX = X - np.tile(M, X.shape()[1])
+        E = 0.5 * sum(DX * (np.dot(inv(S), DX)), axis=0)
+        E = E + 0.5 * M.shape()[0] * np.log(2 * np.pi) + 0.5 * np.log(np.det(S))
+        P = np.exp(-E)
+    elif X.shape()[1] == 1:
+        DX = np.tile(X, M.shape()[1]) - M
+        E = 0.5 * sum(DX * (np.dot(inv(S), DX)), axis=0)
+        E = E + 0.5 * M.shape()[0] * np.log(2 * np.pi) + 0.5 * np.log(np.det(S))
+        P = np.exp(-E)
+    else:
+        DX = X - M
+        E = 0.5 * np.dot(DX.T, np.dot(inv(S), DX))
+        E = E + 0.5 * M.shape()[0] * np.log(2 * np.pi) + 0.5 * np.log(np.det(S))
+        P = np.exp(-E)
+    return (P[0], E[0])
