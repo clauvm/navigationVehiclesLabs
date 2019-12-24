70ee54b0bba2fa22e1881ca83b9133ac5e7c505a
nik1168 <ngeisser32@gmail.com>
2019-12-24 01:27:46 +0100

    Correct generate data

--- kalman_filter/assignment3/NavigationLab3_Files/generate_data_2D.py
+++ kalman_filter/assignment3/NavigationLab3_Files/generate_data_2D.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 '''
 This function generates the data for a constant-velocity 2D motion example.
-The motion is divided in 5 segments, passing through the points: 
+The motion is divided in 5 segments, passing through the points:
   - (200,-100)  to (100,100)
   - (100,100)   to (100,300)
   - (100,300)   to (-200,300)
@@ -22,18 +22,18 @@ import sys
 import numpy as np
 import matplotlib.pyplot as plt
 
-Q1 = float(sys.argv[1]) 
+Q1 = float(sys.argv[1])
 Q2 = float(sys.argv[2])
-R1 = float(sys.argv[3]) 
+R1 = float(sys.argv[3])
 R2 = float(sys.argv[4])
 
 nSegments = 5
 points = np.array([[200, -100],
 		   [100, 100],
-		   [100, 300], 
-		   [-200, 300], 
+		   [100, 300],
+		   [-200, 300],
 		   [-200, -200],
-		   [0,0]], dtype=float)  
+		   [0,0]], dtype=float)
 
 dp = np.diff(points, axis=0)
 dist = dp**2
@@ -42,34 +42,34 @@ dist = np.round(np.sqrt(dist[:,0] + dist[:,1])) # distance
 ang = np.arctan2(dp[:, 1], dp[:, 0]) # orientation
 ang = np.array([ang]).T
 
-NumberOfDataPoints = np.sum(dist)
+NumberOfDataPoints = int(np.sum(dist))
 
 T = 0.5 # [s] Sampling time interval
 
 v_set = 2 * np.hstack((np.cos(ang), np.sin(ang)))
 
 idx = 0
-v = np.kron(np.ones((dist[idx], 1)), v_set[idx, :])
+v = np.kron(np.ones((int(dist[idx]), 1)), v_set[idx, :])
 for idx in range(1, nSegments):
-      v = np.vstack((v, np.kron(np.ones((dist[idx], 1)), v_set[idx, :])))
-      
+      v = np.vstack((v, np.kron(np.ones((int(dist[idx]), 1)), v_set[idx, :])))
+
 
 # ==motion generation====================================================
 
-A = np.array([[1, 0, 0, 0], 
-              [0, 0, 0, 0], 
-              [0, 0, 1, 0], 
-              [0, 0, 0, 0]], dtype=float)  
+A = np.array([[1, 0, 0, 0],
+              [0, 0, 0, 0],
+              [0, 0, 1, 0],
+              [0, 0, 0, 0]], dtype=float)
 
-B = np.array([[T, 0], 
-              [1, 0], 
-              [0, T], 
-              [0, 1]], dtype=float)  
+B = np.array([[T, 0],
+              [1, 0],
+              [0, T],
+              [0, 1]], dtype=float)
 
-G = np.array([[T**2/2, 0], 
-              [T, 0], 
-              [0, T**2/2], 
-              [0, T]], dtype=float)  
+G = np.array([[T**2/2, 0],
+              [T, 0],
+              [0, T**2/2],
+              [0, T]], dtype=float)
 
 w_x = np.random.normal(0.0, np.sqrt(Q1), NumberOfDataPoints) # noise in x-direction
 w_y = np.random.normal(0.0, np.sqrt(Q2), NumberOfDataPoints) # noise in y-direction
@@ -89,7 +89,7 @@ position = x[:,(0,2)] # 2D position data
 z = np.zeros((NumberOfDataPoints, 2))
 for idx in range(0, int(NumberOfDataPoints)):
   z[idx, 0] = np.sqrt(np.dot(position[idx, :], position[idx, :]))
-  z[idx, 1] = np.arctan2(position[idx, 1], position[idx, 0]) 
+  z[idx, 1] = np.arctan2(position[idx, 1], position[idx, 0])
 
 # unwrap radian phases by changing absolute jumps greater than pi to their 2*pi complement
 z[:, 1] = np.unwrap(z[:, 1])
@@ -103,7 +103,7 @@ f1 = plt.figure()
 plt.plot(x[:, 0], x[:, 2], label='linear')
 plt.xlabel('x-axis [m]')
 plt.ylabel('y-axis [m]')
-plt.savefig('xy.pdf') 
+plt.savefig('xy.pdf')
 f1.show()
 
 xlab = [[' '], ['Time step [s]']]
@@ -117,6 +117,6 @@ for idx in range(0, 2):
   plt.ylabel(ylab[idx])
   plt.legend([line_z, line_ze], ['Measured', 'Exact'], fancybox=True, framealpha=0.0, loc='lower center', ncol=2)
   #leg.get_frame().set_linewidth(0.0)
-plt.savefig('r_th.pdf') 
+plt.savefig('r_th.pdf')
 f2.show()
 
